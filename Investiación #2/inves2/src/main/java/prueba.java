73850c31d095c6a5cbacb63636bae0c87afb840a
Iván <ivan.betegon@gmail.com>
2019-10-23 15:42:30 -0500

    .

--- Investiación #2/inves2/src/main/java/prueba.java
+++ Investiación #2/inves2/src/main/java/prueba.java
@@ -2,7 +2,7 @@ import static spark.Spark.*;
 public class prueba {
 
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Carrito.");
+        get("/hello", (req, res) -> "Carrito...");
         
     }
 }
