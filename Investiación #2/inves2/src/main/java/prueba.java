630ef39a99dc8c58e256a5f1b176bb96bd2d3894
Iván <ivan.betegon@gmail.com>
2019-10-29 19:16:06 -0500

    LabFinal

--- Investiación #2/inves2/src/main/java/prueba.java
+++ Investiación #2/inves2/src/main/java/prueba.java
@@ -1,8 +1,43 @@
+
 import static spark.Spark.*;
+
 public class prueba {
 
     public static void main(String[] args) {
+
         get("/hello", (req, res) -> "Probando, uno dos.");
+
+        get("/estudiantes", (req, res) -> {
+
+            res.type("application/json");
+            return "{\"id: 1\", \"nombre: Erick Agrazal\", \"edad: 28\"}, {\"id: 2\", \"nombre: Vicente Lopez\", \"edad: 28\"}, {\"id: 3\", \"nombre: Pedro Navaja\", \"edad: 65\"}";
+        
+        });
+        
+
+        get("/estudiantes/1", (req, res) -> {
+
+            res.type("application/json");
+            return "{\"id: 1\", \"nombre: Erick Agrazal\", \"edad: 28\"}";
+        
+        });
+
+        get("/estudiantes/2", (req, res) -> {
+
+            res.type("application/json");
+            return "{\"id: 2\", \"nombre: Vicente Lopez\", \"edad: 28\"}";
+        
+        });
+
+        get("/estudiantes/3", (req, res) -> {
+
+            res.type("application/json");
+            return "{\"id: 3\", \"nombre: Pedro Navaja\", \"edad: 65\"}";
+        
+        });
+
+
+
         
     }
 }
